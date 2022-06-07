effe580301a73367bbc48127436cac9403772063
chorokezz <82281612+chorokezz@users.noreply.github.com>
2022-06-07 18:59:09 +0100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,237 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">home</a> / about </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/CoopsSolution.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>The cooperatives in Morocco need to integrate the digital world so they can extend their businesses and reach more people</p>
+         <p>to do so they will need to have a website on which they represent their products and sell it online, and then starting a marketing process.</p>
+         <p>First of all we’re talking about more than 27 000 cooperative in Morocco, and they are in different sectors, </p>
+         <p>especially Agriculture and food industry, Artisan and Argan. The better solution is to offer an intelligent and good represented digital market for all of the cooperatives in Morocco,</p>
+         <p>with one access but different designs and presentations (depends to each sector)</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">client's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/woman.png" alt="">
+         <p>je suis satisfaite de ce site , ses outils m'ont aidé à trouver exactement ce que j'ai recherché.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>user1</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/profile.png" alt="">
+         <p>nice website,fast delivery,good quality.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>user2</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/profile.png" alt="">
+         <p>j'aime beaucoup les pelluches qui sont fabriquées par la main.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>user3</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/woman.png" alt="">
+         <p>everything is nice and also the vegetables are so good.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>user4</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/profile.png" alt="">
+         <p>Not bad! Everything is nice except the artisan some of it are not that good but it's okay not a big problem.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>user5</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/woman.png" alt="">
+         <p>Tout est bien je suis très satisfaite pour ce travail. J'éspère qu'il y'a plus que site web comme ce site.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>user6</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="authors">
+
+   <h1 class="title">The cooperative members</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/man.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/female-1024x1024.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/man.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/female-1024x1024.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/man.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/female-1024x1024.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>john deo</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
